406557e1e8de8e785cd83c28fc5cbaeb1aee0515
LucasCSalles <lucascampossalles23@gmail.com>
2026-03-16 16:24:00 -0300

    Fix: fixing package name

--- src/main/java/br/com/fiap/study_apir/Controller/PingController.java
+++ src/main/java/br/com/fiap/study_apir/Controller/PingController.java
@@ -1,4 +1,4 @@
-package br.com.fiap.study_apir;
+package br.com.fiap.study_apir.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +26,6 @@ public class PingController {
      } 
      @GetMapping("/zxply")
      public String zxply() {
-        return "é eu";
+        return "Sou eu";
      }      
 }
-
