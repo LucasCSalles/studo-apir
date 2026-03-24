5defceb2f658592cc41d6906592584dfc49df86e
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-23 22:44:37 -0300

    Add: Adding ProdutoController for new produto class

--- src/main/java/br/com/fiap/study_apir/Controller/PingController.java
+++ src/main/java/br/com/fiap/study_apir/Controller/PingController.java
@@ -1,4 +1,4 @@
-package br.com.fiap.study_apir.controller;
+package br.com.fiap.study_apir.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
